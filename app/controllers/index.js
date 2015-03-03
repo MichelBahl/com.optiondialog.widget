223242dfa645d3420e9752ac0fa5e7ef1fd4bc35
Michael Bahl <michaelbahl@Michaels-MacBook-Pro.local>
2015-03-03 13:30:15 +0100

    Initial commit

---
+++ app/controllers/index.js
@@ -0,0 +1,35 @@
+
+function doDialogOne(e){
+	
+	$.optionDialogOne.show();
+}
+
+function doDialogTwo(e){
+	
+	$.optionDialogTwo.show();
+}
+
+function doDialogThree(e){
+	
+	$.optionDialogThree.show();
+}
+
+function doDialogFour(e){
+	
+	$.optionDialogFour.show();
+}
+
+function doDialogFive(e){
+	
+	$.optionDialogFive.show();
+}
+
+
+//Dialog feedback
+function doClickOptionDialog(e){
+	
+	alert(e.index);
+
+}
+
+$.index.open();
